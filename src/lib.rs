12a1c820183f261057522663b74c6ff0410438d5
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2026-08-11 13:43:55 +0200

    Initial refactoring
    - Bump edition to 2024
    - Reduce code indentation
    - Add additional checks
    - Improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,24 @@
 #![doc = include_str!("../README.md")]
 
 use proc_macro::TokenStream;
-
 use quote::ToTokens;
-use syn::visit_mut::{self, VisitMut};
-use syn::{parse_macro_input, AttrStyle, Expr, Ident, ItemMod, ItemUse, Meta, Signature};
+use syn::{
+    AttrStyle, Expr, Ident, ItemMod, ItemUse, Meta, MetaList, Signature, parse_macro_input,
+    spanned::Spanned,
+    visit_mut::{VisitMut, visit_expr_mut, visit_signature_mut},
+};
 
 #[proc_macro_attribute]
-/// Annotate `use` declarations with this to replace them in syncified output.
+/// Annotate `use` declarations with this to replace them in the syncified copy.
 pub fn syncify_replace(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Avoid usage on items that are not `use` declarations.
+    let clone = item.clone();
+    let _ = parse_macro_input!(clone as ItemUse);
     item
 }
 
 #[proc_macro_attribute]
-/// Produce a sync version of the anotated async function by stripping the `async` keyword and all `.await`s.
+/// Produce a synchronous copy of the annotated module, stripping `async` and `.await`.
 pub fn syncify(attr: TokenStream, item: TokenStream) -> TokenStream {
     let sync_mod_name = parse_macro_input!(attr as Ident);
 
@@ -21,7 +26,17 @@ pub fn syncify(attr: TokenStream, item: TokenStream) -> TokenStream {
 
     let mut sync_copy = input_mod.clone();
     sync_copy.ident = sync_mod_name;
-    SyncifyVisitor.visit_item_mod_mut(&mut sync_copy);
+
+    let mut visitor = SyncifyVisitor::new();
+    visitor.visit_item_mod_mut(&mut sync_copy);
+    if !visitor.errors.is_empty() {
+        return visitor
+            .errors
+            .into_iter()
+            .map(|e| e.to_compile_error())
+            .collect::<proc_macro2::TokenStream>()
+            .into();
+    }
 
     let mut out = proc_macro2::TokenStream::new();
     input_mod.to_tokens(&mut out);
@@ -29,39 +44,72 @@ pub fn syncify(attr: TokenStream, item: TokenStream) -> TokenStream {
     out.into()
 }
 
-struct SyncifyVisitor;
+/// Convert async code into its blocking equivalent:
+/// * `async fn` becomes `fn`
+/// * `expr.await` becomes `expr`
+struct SyncifyVisitor {
+    errors: Vec<syn::Error>,
+}
+
+impl SyncifyVisitor {
+    fn new() -> Self {
+        Self { errors: Vec::new() }
+    }
+}
 
 impl VisitMut for SyncifyVisitor {
     fn visit_expr_mut(&mut self, i: &mut Expr) {
-        match i {
-            Expr::Await(exp) => {
-                *i = *exp.base.clone();
-            }
-            _ => visit_mut::visit_expr_mut(self, i),
+        visit_expr_mut(self, i);
+        if let Expr::Await(expr) = i {
+            *i = *expr.base.clone();
         }
     }
 
     fn visit_signature_mut(&mut self, i: &mut Signature) {
         i.asyncness = None;
+        visit_signature_mut(self, i);
     }
 
-    fn visit_item_use_mut(&mut self, i: &mut syn::ItemUse) {
-        // Look for `syncify_replace` attributes on `use` items, and honour them.
-        for attr in i.attrs.clone().iter() {
-            // Ignore inner attributes
-            if matches!(attr.style, AttrStyle::Outer) {
-                match &attr.meta {
-                    Meta::List(meta_list) => {
-                        if let Some(ident) = meta_list.path.get_ident() {
-                            if ident == "syncify_replace" {
-                                let new_use: ItemUse = syn::parse2(meta_list.tokens.clone()).expect("Content of the syncify_replace macro must be a valid `use` item, for example `use std::collections::HashSet`.");
-                                *i = new_use;
-                            }
-                        }
-                    }
-                    _ => { /* no-op */ }
-                }
+    fn visit_item_use_mut(&mut self, i: &mut ItemUse) {
+        // Honour `syncify_replace` attributes: replace the `use` item with the
+        // replacement it carries, and drop the attribute. Any other attributes are
+        // moved onto the replacement.
+        let mut replacement = None;
+        strip_replace(i, |meta| {
+            let None = replacement else {
+                let err =
+                    syn::Error::new(meta.span(), "syncify_replace attribute used multiple times");
+                self.errors.push(err);
+                return;
+            };
+            match syn::parse2::<ItemUse>(meta.tokens.clone()) {
+                Ok(rep) => replacement = Some(rep),
+                Err(err) => self.errors.push(err),
             }
+        });
+        if let Some(mut replacement) = replacement {
+            replacement.attrs = i.attrs.clone();
+            *i = replacement;
         }
     }
+}
+
+/// Remove `syncify_replace` attributes from `use` item, calling `on_replace` with the replacement token stream and span.
+fn strip_replace(item: &mut ItemUse, mut on_replace: impl FnMut(&MetaList)) {
+    item.attrs.retain_mut(|attr| {
+        if !matches!(attr.style, AttrStyle::Outer) {
+            return true;
+        }
+        let Meta::List(meta_list) = &attr.meta else {
+            return true;
+        };
+        let Some(segment) = meta_list.path.segments.last() else {
+            return true;
+        };
+        if segment.ident != "syncify_replace" {
+            return true;
+        }
+        on_replace(meta_list);
+        false
+    });
 }
