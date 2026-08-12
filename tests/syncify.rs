15ff7cd9d89e94545321a6d69ac0d46f5608b61f
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2026-08-12 12:59:07 +0200

    Add tests for `#[syncify_skip]` and `#[syncify_include]`

--- tests/syncify.rs
+++ tests/syncify.rs
@@ -49,3 +49,58 @@ fn replace_use_item() {
     assert_eq!(replace_sync::get(), 2);
     assert_eq!(block_on(replace::get()), 1);
 }
+
+#[syncify(only_sync)]
+mod only {
+    #[syncify::syncify_skip]
+    pub async fn async_only_val() -> u32 {
+        1
+    }
+
+    #[syncify::syncify_include]
+    pub fn blocking_only_val() -> u32 {
+        2
+    }
+}
+
+#[test]
+fn skip_keeps_item_in_original() {
+    assert_eq!(block_on(only::async_only_val()), 1);
+}
+
+#[test]
+fn include_moves_item_to_sync() {
+    assert_eq!(only_sync::blocking_only_val(), 2);
+}
+
+#[syncify(marker_impl_sync)]
+mod marker_impl {
+    pub struct State(pub i32);
+
+    impl State {
+        #[syncify::syncify_skip]
+        pub async fn async_get(&self) -> i32 {
+            self.0
+        }
+
+        #[syncify::syncify_include]
+        pub fn blocking_get(&self) -> i32 {
+            self.0
+        }
+
+        pub async fn both_get(&self) -> i32 {
+            self.0
+        }
+    }
+}
+
+#[test]
+fn impl_marker_routing() {
+    let blocking_state = marker_impl_sync::State(3);
+    assert_eq!(blocking_state.both_get(), 3);
+    assert_eq!(blocking_state.blocking_get(), 3);
+
+    let asynch_state = marker_impl::State(3);
+    assert_eq!(block_on(asynch_state.both_get()), 3);
+    assert_eq!(block_on(asynch_state.async_get()), 3);
+}
